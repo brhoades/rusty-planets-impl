fe6b03ca87711b5b0b62c17b5da0a82ad39e526e
Billy Rhoades <me@brod.es>
2020-01-07 19:28:14 -0800

    Added planet rendering that's centered.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 mod planet;
 use piston_window::*;
-use planet::{World,PhysicsFrame};
+use planet::{World,PhysicsFrame,Planet,Star};
 
 fn main() {
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
+        WindowSettings::new("Rusty Planets", [640, 480])
         .exit_on_esc(true).build().unwrap();
 
     let mut world = World{entities: vec!()};
+    world.entities.push(Star::new(window.size()));
 
     while let Some(event) = window.next() {
-        world = window.draw_2d(&event, |context, graphics, _device| {
+        // In-order physics state of next frame
+        let frames = world.entities
+            .iter()
+            .map(|e| e.tick(&world))
+            .collect::<Vec<_>>();
+
+        // apply frames
+        world.entities
+            .iter_mut()
+            .zip(frames)
+            .map(|(e, f)| e.set(f))
+            .for_each(drop); // drain to evaluate lazy iter
+
+        window.draw_2d(&event, |context, graphics, _device| {
             clear([0.1, 0.1, 0.1, 1.0], graphics);
-
-            let frames = world.entities.iter().map(|e| e.tick(&world)).collect::<Vec<PhysicsFrame>>();
-
-            for (i, e) in world.entities.iter_mut().enumerate() {
-                e.set(&frames[i]);
-            }
-
             for e in &world.entities {
                 e.render(&context, graphics);
             }
 
-            // world was moved in and now out of closure
-            world
-        }).expect("Error rendering world.");
+            // world was moved in, now move it out of the closure
+            // (try removing this)
+        });
     }
 }
--- src/planet.rs
+++ src/planet.rs
@@ -5,11 +5,14 @@ pub trait Renderable {
 }
 
 pub trait PhysicsBody {
+    fn position(&self) -> Motion;
     fn tick(&self, w: &World) -> PhysicsFrame;
-    fn set(&mut self, f: &PhysicsFrame);
+    fn set(&mut self, f: PhysicsFrame);
 }
 
-pub struct PhysicsFrame {
+pub type PhysicsFrame = Motion;
+
+pub struct Motion {
     velocity: [f64; 2],
     position: [f64; 2],
 }
@@ -25,26 +28,37 @@ pub struct World {
 pub struct Planet {
     velocity: [f64; 2],
     position: [f64; 2],
-    color: [f64; 4],
-    size: [f64; 2],
+    color: [f32; 4],
+    size: f64,
 }
 
-/*
 impl Planet {
-    fn new(pos: [f64; 2], vel: [f64; 2]) -> Self {
+    // makes a new planet that's (theoretically) stable around other at height at a period (% from 0 degrees).
+    fn new_stable_orbit(other: &Box<dyn PhysicsBody>, height: f64, period: f64, size: f64, color: [f32; 4]) -> Self {
+        let oPos = other.position();
+
         Planet{
-            color:
+            velocity: [1.0, 0.0],
+            position: [oPos.position[0] + height, oPos.position[1] + height],
+            color,
+            size,
         }
     }
 }
-*/
+
+fn get_centered_square_extents(center: &[f64; 2], size: f64) -> [f64; 4] {
+    return ellipse::centered([center[0], center[1], size / 2.0, size / 2.0]);
+}
 
 impl Renderable for Planet {
     fn render(&self, context: &Context, graphics: &mut G2d) {
-        rectangle([1.0, 0.0, 0.0, 1.0],
-                  [0.0, 0.0, 100.0, 100.0],
-                  context.transform,
-                  graphics);
+        let extents = ellipse::circle(self.position[0], self.position[1], self.size);
+        rectangle(
+            self.color,
+            extents,
+            context.transform,
+            graphics
+        );
     }
 }
 
@@ -56,11 +70,68 @@ impl PhysicsBody for Planet {
         }
     }
 
-    fn set(&mut self, f: &PhysicsFrame) {
+    fn set(&mut self, f: PhysicsFrame) {
         self.position = f.position;
         self.velocity = f.velocity;
+    }
+
+    fn position(&self) -> Motion {
+        Motion {
+            position: self.position,
+            velocity: [0.0, 0.0],
+        }
     }
 }
 
 pub struct Star {
+    position: [f64; 2],
+    color: [f32; 4],
+    size: f64,
+}
+
+impl Star {
+    pub fn new(window_size: Size) -> Box<Star> {
+        Box::new(Star{
+            position: [window_size.width / 2.0, window_size.height / 2.0],
+            color: [1.0, 0.5, 0.5, 1.0],
+            size: 10.0,
+        })
+    }
+}
+
+impl PhysicsBody for Star {
+    // Let's pretend the star doesn't move
+    fn tick(&self, _w: &World) -> PhysicsFrame {
+        PhysicsFrame {
+            velocity: [0.0, 0.0],
+            position: self.position,
+        }
+    }
+
+    fn set(&mut self, _f: PhysicsFrame) {}
+
+    fn position(&self) -> Motion {
+        Motion {
+            position: self.position,
+            velocity: [0.0, 0.0],
+        }
+    }
+}
+
+impl Renderable for Star {
+    fn render(&self, context: &Context, graphics: &mut G2d) {
+        let extents = ellipse::circle(self.position[0], self.position[1], self.size);
+        rectangle(
+            self.color,
+            extents,
+            context.transform,
+            graphics
+        );
+    }
+}
+
+impl Entity for Star {
+    fn name(&self) -> &'static str {
+        "Star"
+    }
 }

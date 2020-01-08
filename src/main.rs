cddf7b83c1c94dfee956e9eaaf50d93d043e1e0e
Billy Rhoades <me@brod.es>
2020-01-07 20:25:46 -0800

    Added mass fucntion, tried scaling G.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod planet;
 use piston_window::*;
 use planet::{World,PhysicsFrame,Planet,Star};
+use std::time::{Instant,Duration};
 
 fn main() {
     let mut window: PistonWindow =
@@ -14,17 +15,18 @@ fn main() {
             &world.entities[0],
             250.0,
             0.0,
-            1000.0,
+            10.0,
             10.0,
             [1.0; 4],
         )
     );
 
+    let mut last = Instant::now();
     while let Some(event) = window.next() {
         // In-order physics state of next frame
         let frames = world.entities
             .iter()
-            .map(|e| e.tick(&world))
+            .map(|e| e.tick(&world, last.elapsed()))
             .collect::<Vec<_>>();
 
         // apply frames
@@ -34,6 +36,9 @@ fn main() {
             .map(|(e, f)| e.set(f))
             .for_each(drop); // drain to evaluate lazy iter
 
+        last = Instant::now();
+
+        std::thread::sleep(Duration::from_millis(1000/60));
         window.draw_2d(&event, |context, graphics, _device| {
             clear([0.1, 0.1, 0.1, 1.0], graphics);
             for e in &world.entities {
--- src/planet.rs
+++ src/planet.rs
@@ -1,12 +1,15 @@
 use piston_window::*;
+use std::time::Duration;
 
 pub trait Renderable {
     fn render(&self, ctx: &Context, graphics: &mut G2d);
 }
 
 pub trait PhysicsBody {
-    fn position(&self) -> Motion;
-    fn tick(&self, w: &World) -> PhysicsFrame;
+    fn motion(&self) -> Motion;
+    fn mass(&self) -> f64;
+
+    fn tick(&self, _w: &World, d: Duration) -> PhysicsFrame;
     fn set(&mut self, f: PhysicsFrame);
 }
 
@@ -33,11 +36,14 @@ pub struct Planet {
     mass: f64,
 }
 
+// const G: f64 = 6.67430e-11;
+const G: f64 = 1.0;
+
 impl Planet {
     // makes a new planet that's (theoretically) stable around other at height at a period (% from 0 degrees).
     pub fn new_stable_orbit(other: &Box<dyn Entity>, height: f64, period: f64, mass: f64, size: f64, color: [f32; 4]) -> Box<Self> {
-        let o_pos = other.position();
-        let mu = 6.67430e-11*size;
+        let o_pos = other.motion();
+        let mu = G * mass;
         let v = (mu*(2.0 / height - 1.0 / height)).sqrt();
 
         Box::new(Planet{
@@ -73,9 +79,25 @@ impl Renderable for Planet {
 }
 
 impl PhysicsBody for Planet {
-    fn tick(&self, w: &World) -> PhysicsFrame {
+    fn tick(&self, w: &World, d: Duration) -> PhysicsFrame {
+        let mut dv = [0.0; 2];
+
+        for e in &w.entities {
+            let o_mass = e.mass();
+            let position = self.motion().position;
+            let o_position = e.motion().position;
+            let dist_sq = (o_position[0] - position[0]).powf(2.0) + (o_position[1] - position[1]).powf(2.0);
+            let force = G * o_mass * self.mass() / dist_sq * d.as_secs_f64();
+
+            println!("dv: {:?}, pos: {:?}", dv, self.position);
+            // for now, just add this to our x
+            dv[0] += force;
+        }
+
+        println!("dv: {:?}, pos: {:?}", dv, self.position);
+
         PhysicsFrame {
-            velocity: self.velocity,
+            velocity: [self.velocity[0] + dv[0], self.velocity[1] + dv[1]],
             position: [self.position[0] + self.velocity[0], self.position[1] + self.velocity[1]],
         }
     }
@@ -85,11 +107,15 @@ impl PhysicsBody for Planet {
         self.velocity = f.velocity;
     }
 
-    fn position(&self) -> Motion {
+    fn motion(&self) -> Motion {
         Motion {
             position: self.position,
             velocity: [0.0, 0.0],
         }
+    }
+
+    fn mass(&self) -> f64 {
+        self.mass
     }
 }
 
@@ -105,7 +131,8 @@ impl Star {
         Box::new(Star{
             position: [window_size.width / 2.0, window_size.height / 2.0],
             color: [1.0, 0.5, 0.5, 1.0],
-            mass: 1.989e30,
+            // mass: 1.989e30,
+            mass: 1000.0,
             size: 20.0,
         })
     }
@@ -113,7 +140,7 @@ impl Star {
 
 impl PhysicsBody for Star {
     // Let's pretend the star doesn't move
-    fn tick(&self, _w: &World) -> PhysicsFrame {
+    fn tick(&self, _w: &World, d: Duration) -> PhysicsFrame {
         PhysicsFrame {
             velocity: [0.0, 0.0],
             position: self.position,
@@ -122,11 +149,15 @@ impl PhysicsBody for Star {
 
     fn set(&mut self, _f: PhysicsFrame) {}
 
-    fn position(&self) -> Motion {
+    fn motion(&self) -> Motion {
         Motion {
             position: self.position,
             velocity: [0.0, 0.0],
         }
+    }
+
+    fn mass(&self) -> f64 {
+        self.mass
     }
 }
 
